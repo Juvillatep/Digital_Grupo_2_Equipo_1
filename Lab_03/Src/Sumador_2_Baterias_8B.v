bd4e2c8c35ad7ec20e5844e1449a228f5658b4e4
Juvillatep <72473318+Juvillatep@users.noreply.github.com>
2025-01-06 23:42:36 -0500

    Update Sumador_2_Baterias_8B.v

--- Lab_03/Src/Sumador_2_Baterias_8B.v
+++ Lab_03/Src/Sumador_2_Baterias_8B.v
@@ -2,44 +2,28 @@
 `include "Sumador_4B.v"
 
 module SUMADOR_8BITS(
-    input [3:0] A,B,
+    input [7:0] A,B,
     input Ci,
-    output [3:0] Sum,
+    output [7:0] Sum,
     output Co
 );
 
 wire Ci1, Ci2, Ci3;
 
-SUMADOR_1BIT SUM1(
-    .A(A[0]),
-    .B(B[0]),
+SUMADOR_4BITS SUM1(
+    .A(A[3:0]),
+    .B(B[3:0]),
     .Ci(Ci),
-    .Sum(Sum[0]),
+    .Sum(Sum[3:0]),
     .Co(Ci1)
 );
 
-SUMADOR_1BIT SUM2(
-    .A(A[1]),
-    .B(B[1]),
+SUMADOR_4BITS SUM2(
+    .A(A[7:4]),
+    .B(B[7:4]),
     .Ci(Ci1),
-    .Sum(Sum[1]),
+    .Sum(Sum[7:4]),
     .Co(Ci2)
-);
-
-SUMADOR_1BIT SUM3(
-    .A(A[2]),
-    .B(B[2]),
-    .Ci(Ci2),
-    .Sum(Sum[2]),
-    .Co(Ci3)
-);
-
-SUMADOR_1BIT SUM4(
-    .A(A[3]),
-    .B(B[3]),
-    .Ci(Ci3),
-    .Sum(Sum[3]),
-    .Co(Co)
 );
 
 endmodule
